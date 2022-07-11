e34f69c41652bfd0bb6652e1cc7fe4eb36900cab
Chrystian Farias <chrystian.farias@outlook.com>
2022-07-11 10:46:20 -0300

    Query

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 const server = require('http').createServer(app);
 const port = process.env.PORT || 3001;
 const api = process.env.NGROK || "https://7cc7-2804-ef4-561f-a401-50a6-7b38-a862-6a11.sa.ngrok.io/webhook";
-app.get("/", (req,res) => {
-    axios.get(api, {params: req.query})
-        .then(apires => res.send(apires.data))
+app.get("/", async (req,res) => {
+    const apires = await axios.get(api, {params: req.query})
         .catch(console.error);
+    console.log(apires.data); 
+    return res.send(apires.data);
 });
 
 server.listen(port, () =>
