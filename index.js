3a2cf90c35bdbdf5fa0e02bd94b7b5bf295c457c
Chrystian Farias <chrystian.farias@outlook.com>
2022-07-11 11:14:12 -0300

    Post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const server = require('http').createServer(app);
 const port = process.env.PORT || 3001;
 const api = process.env.NGROK || "https://7cc7-2804-ef4-561f-a401-50a6-7b38-a862-6a11.sa.ngrok.io/webhook";
@@ -11,7 +12,8 @@ app.get("/", async (req,res) => {
     return res.status(apires.status).send(apires.data);
 });
 app.post("/", async (req,res) => {
-    const apires = await axios.post(api, req)
+    console.log('Facebook request body:', req.body);
+    const apires = await axios.post(api, req.body)
         .catch(console.error);
     return res.status(apires.status).send(apires.data);
 });
