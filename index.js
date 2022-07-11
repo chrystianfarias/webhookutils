bcfbf0bb042eb1f9098964e2e32cdb1f075c1fc7
Chrystian Farias <chrystian.farias@outlook.com>
2022-07-11 10:36:17 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const port = process.env.PORT || 3001;
+const api = process.env.NGROK || "https://7cc7-2804-ef4-561f-a401-50a6-7b38-a862-6a11.sa.ngrok.io/webhook";
+app.get("/", (req,res) => {
+    axios.get(api, {params: req.params})
+        .catch(console.error);
+    res.send("");
+});
+
+server.listen(port, () =>
+  console.log(`servidor está rodando na porta ${port}`),
+);
+
+module.exports = server;
