dd969bf9212bd01efa1160cfaa26ff55d5607566
Chrystian Farias <36011789+chrystianfarias@users.noreply.github.com>
2022-08-22 15:47:09 -0300

    Back

--- index.js
+++ index.js
@@ -9,44 +9,27 @@ const api = process.env.NGROK;
 // parse application/json
 app.use(bodyParser.json())
 
-app.get("/webhook", async (req,res) => {
-        let mode = req.query["hub.mode"];
-        let token = req.query["hub.verify_token"];
-        let challenge = req.query["hub.challenge"];
-        let obj = req.body.object;
-
-        // Check if a token and mode were sent
-        if (mode && token) {
-          // Check the mode and token sent are correct
-          if (mode === "subscribe" && token === "208gt") {
-            // Respond with 200 OK and challenge token from the request
-            console.log("WEBHOOK_VERIFIED");
-            return res.status(200).send(challenge);
-          } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            return res.sendStatus(403);
-          }
-        }
+app.get("/", async (req,res) => {
+    try
+    {
+        const apires = await axios.get(api, {params: req.query})
+            .catch(console.error);
+        return res.status(apires.status).send(apires.data);
+    }catch(err)
+    {
+        return res.status(500).send(err);
+    }
 });
-app.post("/webhook", async (req,res) => {
-
-        let mode = req.query["hub.mode"];
-        let token = req.query["hub.verify_token"];
-        let challenge = req.query["hub.challenge"];
-        let obj = req.body.object;
-
-        // Check if a token and mode were sent
-        if (mode && token) {
-          // Check the mode and token sent are correct
-          if (mode === "subscribe" && token === "208gt") {
-            // Respond with 200 OK and challenge token from the request
-            console.log("WEBHOOK_VERIFIED");
-            return res.status(200).send(challenge);
-          } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            return res.sendStatus(403);
-          }
-        }
+app.post("/", async (req,res) => {
+    try
+    {
+        const apires = await axios.post(api, req.body)
+            .catch(console.error);
+        return res.status(apires.status).send(apires.data);
+    }catch(err)
+    {
+        return res.status(500).send(err);
+    }
 });
 
 server.listen(port, () =>
