cbab5f89200b7db6ce453ff3a142ac111dd88030
Chrystian Farias <36011789+chrystianfarias@users.noreply.github.com>
2022-08-22 15:41:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/webhook", async (req,res) => {
         // Check if a token and mode were sent
         if (mode && token) {
           // Check the mode and token sent are correct
-          if (mode === "subscribe" && token === this.options.verifyToken) {
+          if (mode === "subscribe" && token === "208gt") {
             // Respond with 200 OK and challenge token from the request
             console.log("WEBHOOK_VERIFIED");
             return res.status(200).send(challenge);
@@ -38,7 +38,7 @@ app.post("/webhook", async (req,res) => {
         // Check if a token and mode were sent
         if (mode && token) {
           // Check the mode and token sent are correct
-          if (mode === "subscribe" && token === this.options.verifyToken) {
+          if (mode === "subscribe" && token === "208gt") {
             // Respond with 200 OK and challenge token from the request
             console.log("WEBHOOK_VERIFIED");
             return res.status(200).send(challenge);
